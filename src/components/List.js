3e1eeca44574540284511aedf6c9116df3029ce4
Pedro <pedro7valla@hotmail.com>
2022-01-10 23:06:45 -0500

    First React CORE exercise

---
+++ src/components/List.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+class List extends Component {
+    render() {
+        return (
+            <>
+                <h3>Things I need to do:</h3>
+                <ul>
+                    <li>Learn a lot of React</li>
+                    <li>Climb Everest maybe</li>
+                    <li>Run a marathon never XD</li>
+                    <li>Feed dogs always</li>
+                </ul>
+            </>
+        );
+
+    }
+}
+export default List;
---
+++ src/components/ToDo.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class ToDo extends Component{
+    render(){
+        return (
+            <h1>Hola Dojo!!</h1>
+        );
+    }
+}
+export default ToDo;
