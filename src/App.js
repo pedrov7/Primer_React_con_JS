33a54cd92f69867d9d401b91eb4a8307843eeba9
Pedro <pedro7valla@hotmail.com>
2022-01-11 00:28:52 -0500

    Ejercicio Core y practice con ejercio de propiedades

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@
 import './App.css';
 import ToDo from './components/ToDo'
 import List from './components/List'
+import PersonCard from './components/PersonCard'
 
 function App() {
   return (
     <div className="App">
-      <ToDo/>    
-      <List/>
+      {/* <ToDo/>    
+      <List/> */}
+
+      <PersonCard lastName = {"Valladares"} firstName = { "Pedro"} age = {28} hColor = "Black"/>
+      
+      <PersonCard lastName = {"Valladares"} firstName = { "Jhon"} age = {58} hColor = "Black and White"/>
+      
+      <PersonCard lastName = {"Romero"} firstName = { "Mariana"} age = {57} hColor = "Black and White"/>
+      
+      <PersonCard lastName = {"Mader"} firstName = { "Mateo"} age = {19} hColor = "Brown"/>
+      
+
+    
 
       
     </div>
